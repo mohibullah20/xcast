24060e7cb150241d9c4db9c0365e576bf8708f67
Gabor Szabo <gabor@szabgab.com>
2016-09-30 13:46:42 +0300

    generate pages for the guests

--- xcast.py
+++ xcast.py
@@ -1,10 +1,12 @@
 import argparse
 import json
+import os
 from jinja2 import Environment, PackageLoader
 
 # TODO: fetch source and create the first n entries
 # TODO: generate html
 def main():
+
     with open('data/sources.json') as fh:
         sources = json.load(fh)
 
@@ -18,10 +20,42 @@ def main():
         for s in sources:
             print('{:20} {}'.format(s.get('name', ''), s.get('title', '')))
     elif args.html:
+        episodes = []
+        people = {}
+        for s in sources:
+            print("Processing source {}".format(s['name']))
+            file = 'data/' + s['name'] + '.json'
+            if os.path.exists(file):
+                with open(file) as fh:
+                    try:
+                        episodes.extend(json.load(fh))
+                    except json.decoder.JSONDecodeError as e:
+                        print("ERROR: Could not read in {}".format(file))
+                        print(e)
+                        pass
+
+        for e in episodes:
+            for g in e['guests'].keys():
+                if g not in people:
+                    people[g] = {
+                        'info': e['guests'][g],
+                        'episodes' : []
+                    }
+                people[g]['episodes'].append(e)
+            #print(e)
+
         env = Environment(loader=PackageLoader('xcast', 'templates'))
-        template = env.get_template('index.html')
+        person_template = env.get_template('person.html')
+        if not os.path.exists('html/p/'):
+            os.mkdir('html/p/')
+        for p in people.keys():
+            print(p)
+            with open('html/p/' + p, 'w') as fh:
+                fh.write(person_template.render(name = p))
+
+        main_template = env.get_template('index.html')
         with open('html/index.html', 'w') as fh:
-            fh.write(template.render(sources = sources))
+            fh.write(main_template.render(sources = sources))
     else:
         parser.print_help()
 
